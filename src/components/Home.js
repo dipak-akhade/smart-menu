1f60dfed5ba612d08c8791c7e57141483dd242a2
dipak-akhade <30806862+dipak-akhade@users.noreply.github.com>
2023-03-27 00:22:07 +0530

    Integration of Raw API response in home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,42 +1,38 @@
-import React from 'react';
-import homedata from './home.json';
-import './Home.css';
-
+import React from "react";
+import homedata from "./home.json";
+import apidata from "./raw_api_response.json";
+import "./Home.css";
 
 const Homedata = homedata.homemenu;
+const ApiData = apidata;
 export default function Home() {
+  const categories = Object.keys(ApiData.Categories);
+
   return (
     <div>
-       {
-          
-          
-          Homedata.map((records,i)=>{
-              return(
-
-                  <> 
-                 <div className='container my-3'>
-                    <div className='row box boxshadow m-3'>
-                    <div >
-                      <h5>{records.category}</h5>
-                    </div>
-                        <div className='row'>
-                            <div>
-                            <p>{records.dishes.slice(0,70)}.........</p>
-                            <a href={records.url} class="btn btn-primary btn-small">Read More</a>
-                            </div>
-                     </div>
-                    </div>
-     
+      {categories.map((records, i) => {
+        return (
+          <>
+            <div className="container my-3">
+              <div className="row box boxshadow m-3">
+                <div>
+                  <h5>{records}</h5>
+                </div>
+                <div className="row">
+                  <div>
+                    <p>
+                      {Object.keys(apidata.Categories[records])
+                        .join(", ")
+                        .slice(0, 100)}
+                      ......
+                    </p>
                   </div>
-
-
-
-
-
-                  </>   
-              )
-          })
-      }
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      })}
     </div>
   );
 }
