81abc45929243e8f4eaf87d1f33adc708163c47f
dipak-akhade <30806862+dipak-akhade@users.noreply.github.com>
2023-03-29 11:01:27 +0530

    Home page UI upgrade

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,15 @@ import React from "react";
 import homedata from "./home.json";
 import apidata from "./raw_api_response.json";
 import "./Home.css";
-import { Card, Avatar, Icon, Button, Divider } from "antd";
+import { Card, Avatar, Icon, Button, Space, Divider } from "antd";
 const { Meta } = Card;
 
 const Homedata = homedata.homemenu;
 const ApiData = apidata;
+const ApiParsedData = JSON.parse(JSON.stringify(apidata));
 export default function Home() {
   const categories = Object.keys(ApiData.Categories);
-
+  console.log(ApiParsedData.Categories["Breakfast"].Image);
   return (
     <div>
       {categories.map((records, i) => {
@@ -17,24 +18,31 @@ export default function Home() {
           <Card
             bodyStyle={{ background: "Gray" }}
             onClick={() => alert("Clicked - " + records)}
-            title={records}
             bordered={true}
             size={"small"}
-            style={{ width: 400 }}
+            style={{ width: "auto", padding: "10px 10px" }}
             hoverable
             cover={
               <img
-                alt="example"
-                src="https://assetsnffrgf-a.akamaihd.net/assets/m/1102015169/univ/art/1102015169_univ_lsr_lg.jpg"
+                alt="WhatsMenu"
+                src={ApiParsedData.Categories[records].Image}
+                height={"auto"}
+                title="CategoryImage"
               />
             }
           >
+            <h2>{records}</h2>
             <p>
               {Object.keys(apidata.Categories[records])
                 .join(", ")
                 .slice(0, 100)}
-              ......
+              .....
             </p>
+            <Space direction="vertical" style={{ width: "100%" }}>
+              <Button type="primary" block>
+                Go to {records}
+              </Button>
+            </Space>
           </Card>
         );
       })}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,104 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Navbar extends Component {
-
-
   render() {
     return (
       <div>
-        
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">Smart Menu</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/">Home</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/Aboutus">About Us</a>
-                    </li>
-                      <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Menu
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">
+              WhatsMenu
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/Aboutus">
+                    About Us
+                  </a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="/"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Menu
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li>
+                      <a className="dropdown-item" href="/SoupandSalad">
+                        Soups & Salads
                       </a>
-                      <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                              <li><a className="dropdown-item" href="/SoupandSalad">Soups & Salads</a></li>
-                              <li><a className="dropdown-item" href="/Starters">Starters</a></li>
-                              <li><a className="dropdown-item" href="/Breakfast">Breakfast</a></li>
-                              <li ><a className="dropdown-item" href="/MainCourse">Main Course</a></li>
-                              <li ><a className="dropdown-item" href="/Rice&biryani">Rice & biryani</a></li>
-                              <li><a className="dropdown-item" href="/Pizza&Pasta">Pizza & Pasta</a></li>
-                              <li><a className="dropdown-item" href="/Noodles">Noodles</a></li>
-                              <li><a className="dropdown-item" href="/Burger&Sandwitches">Burger & Sandwitches</a></li> 
-                              <li ><a className="dropdown-item" href="/Desserts">Desserts</a> </li>
-                      </ul>
-                   </li>
-                    
-                </ul>
-               
-              </div>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="/Starters">
+                        Starters
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="/Breakfast">
+                        Breakfast
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="/MainCourse">
+                        Main Course
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="/Rice&biryani">
+                        Rice & biryani
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="/Pizza&Pasta">
+                        Pizza & Pasta
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="/Noodles">
+                        Noodles
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="/Burger&Sandwitches">
+                        Burger & Sandwitches
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="/Desserts">
+                        Desserts
+                      </a>{" "}
+                    </li>
+                  </ul>
+                </li>
+              </ul>
             </div>
+          </div>
         </nav>
-
       </div>
     );
   }
 }
-
